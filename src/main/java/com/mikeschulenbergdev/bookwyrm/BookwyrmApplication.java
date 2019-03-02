4170b419d7fd89c9920aef22090640d869013381
Mike Schulenberg <mike.schulenberg@gmail.com>
2019-03-02 17:26:29 -0600

    initial commit

---
+++ src/main/java/com/mikeschulenbergdev/bookwyrm/BookwyrmApplication.java
@@ -0,0 +1,13 @@
+package com.mikeschulenbergdev.bookwyrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwyrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwyrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikeschulenbergdev/bookwyrm/BookwyrmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mikeschulenbergdev.bookwyrm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookwyrmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
