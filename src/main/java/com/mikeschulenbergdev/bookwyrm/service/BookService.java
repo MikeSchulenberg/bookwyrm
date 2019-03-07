c4f29db123289ff12ec8c1f0737dcd59e1251d9e
Mike Schulenberg <mike.schulenberg@gmail.com>
2019-03-07 15:30:18 -0600

    create BookService interface

---
+++ src/main/java/com/mikeschulenbergdev/bookwyrm/service/BookService.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (C) 2019 Mike Schulenberg
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package com.mikeschulenbergdev.bookwyrm.service;
+
+import java.util.List;
+
+import com.mikeschulenbergdev.bookwyrm.entity.Book;
+
+/**
+ * Interface for a Service to handle interactions between the
+ * BookRestController and BookDAO classes.
+ * 
+ * @author Mike Schulenberg
+ * @version 0.0.1-SNAPSHOT
+ *
+ */
+public interface BookService {
+
+	public List<Book> findAll();
+	
+	public Book findByID(int id);
+	
+	public void save(Book book);
+	
+	public void deleteByID(int id);
+	
+}
